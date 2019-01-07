67466a0c849e1685119ef122ee439c539d46c7b1
Derrick Sebesta <dsebesta@gmail.com>
2019-01-07 14:06:39 -0800

    added d4d form

--- server.js
+++ server.js
@@ -3,9 +3,15 @@ const app = express();
 const port = 3000;
 const path = require('path');
 
+app.use(express.static(__dirname));
+app.use(express.static('d4d'));
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.get('/d4d', function(req, res) {
+    res.sendFile(path.join(__dirname + '/d4d/index.html'));
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
