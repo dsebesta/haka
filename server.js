1834f44798248f42bc20806369000f44ad0d20e8
Derrick Sebesta <dsebesta@gmail.com>
2019-01-07 14:55:25 -0800

    added referral link

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const path = require('path');
 
 app.use(express.static(__dirname));
 app.use(express.static('d4d'));
+app.use(express.static('ref'));
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
@@ -12,6 +13,10 @@ app.get('/', function(req, res) {
 
 app.get('/d4d', function(req, res) {
     res.status(301).redirect("https://forms.zohopublic.com/pbhllc/form/DrivingForDollars/formperma/47zEPeEOb4z2P2-cWnLBZ4nb7UTprv1D_T0Pl55nvRM")
+});
+
+app.get('/ref', function(req, res) {
+    res.status(301).redirect("https://forms.zohopublic.com/pbhllc/form/Referrala/formperma/l81eW63erViL21YcKaR8Be3KC-OUH5iux7KTQjAU14k")
 });
 
 
